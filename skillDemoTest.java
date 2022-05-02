7ee9f9d904f0c9a624b4fff03a9a28c5b42ccc5b
jemilparikh <jemilparikh@users.noreply.github.com>
2022-05-01 21:17:58 -0700

    Test and Method for multiplication created

---
+++ skillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void multiplyTest() {
+        assertEquals(2, skillDemo.multiply(2, 2));
+    }
+}
+
